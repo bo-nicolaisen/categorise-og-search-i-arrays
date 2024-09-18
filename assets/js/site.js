7b7491adef3063d6f2cf24e6de96663d191a5bb2
Bo Nicolajsen <>
2024-09-18 15:30:00 +0200

    3

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,11 @@
 const frugtOgGront = [
-    "Æble", "Pære", "Blomme", "Banan", "Jordbær",
-    "Hindbær", "Solbær", "Kirsebær", "Vindrue", "Stikkelsbær",
-    "Kartoffel", "Gulerod", "Løg", "Rødbede", "Agurk",
-    "Tomat", "Peberfrugt", "Spinat", "Broccoli", "Blomkål",
-    "Rosenkål", "Majs", "Græskar", "Squash", "Porre",
-    "Spidskål", "Hvidkål", "Salat", "Ærter", "Radise",
-    "Rabarber"
+    "æble", "pære", "blomme", "banan", "jordbær",
+    "hindbær", "solbær", "kirsebær", "vindrue", "stikkelsbær",
+    "kartoffel", "gulerod", "løg", "rødbede", "agurk",
+    "tomat", "peberfrugt", "spinat", "broccoli", "blomkål",
+    "rosenkål", "majs", "græskar", "squash", "porre",
+    "spidskål", "hvidkål", "salat", "ærter", "radise",
+    "rabarber"
 ];
 
 let myScore = 0
@@ -13,8 +13,8 @@ let myScore = 0
 let answer = ""
 newGame()
 
-
 function newGame() {
+    document.getElementById('svar').value = ""
     answer = getRandomItem(frugtOgGront)
     console.log(answer);
 }
@@ -32,16 +32,28 @@ function guessCallback() {
 
 
 
-    // toLowerCase er en string method, der konverterer alle bogstaver til smp, så man kan sammenligne dem uden at få fejl ved store og små bogstaver.
-    let userInput = document.getElementById('svar').value.toLowerCase()
-    answer = answer.toLowerCase()
+
 
 
 
     //--------------------------------------------------------------------
     /* du skal skrive denne del af  funktionen om, så brugeren bare skal gætte en af elementerne i arrayen, så det ikke er så svært!!*/
 
-    if (userInput == answer) {
+    /*  if (userInput == answer) {
+         document.getElementById('feedback').innerHTML = `du har svarret ${userInput}..<br> og det er RIGTIGT!`
+     } else {
+         document.getElementById('feedback').innerHTML = `HA HA! forkert.<br> jeg tænkte på ${answer}`
+     } */
+
+
+    // toLowerCase er en string method, der konverterer alle bogstaver til smp, så man kan sammenligne dem uden at få fejl ved store og små bogstaver.
+
+    let userInput = document.getElementById('svar').value //.toLowerCase()
+    answer = answer.toLowerCase()
+
+    console.log(frugtOgGront.includes(userInput));
+
+    if (frugtOgGront.includes(userInput)) {
         document.getElementById('feedback').innerHTML = `du har svarret ${userInput}..<br> og det er RIGTIGT!`
     } else {
         document.getElementById('feedback').innerHTML = `HA HA! forkert.<br> jeg tænkte på ${answer}`
