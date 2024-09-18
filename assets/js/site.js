70648752de50ac37b8b429067e9cf6a6ccd519e2
Bo Nicolajsen <>
2024-09-18 15:46:02 +0200

    4

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,14 @@
-const frugtOgGront = [
-    "æble", "pære", "blomme", "banan", "jordbær",
-    "hindbær", "solbær", "kirsebær", "vindrue", "stikkelsbær",
-    "kartoffel", "gulerod", "løg", "rødbede", "agurk",
-    "tomat", "peberfrugt", "spinat", "broccoli", "blomkål",
-    "rosenkål", "majs", "græskar", "squash", "porre",
-    "spidskål", "hvidkål", "salat", "ærter", "radise",
-    "rabarber"
+const baseArray = [
+    ["æble", "pære", "blomme", "banan"], ["jordbær",
+        "hindbær", "solbær", "kirsebær"], ["vindrue", "stikkelsbær",
+        "kartoffel", "gulerod", "rødbede"], ["agurk",
+        "tomat", "peberfrugt", "spinat"], [
+        "rosenkål", "majs", "græskar"], ["squash", "porre",
+        "spidskål", "hvidkål"], ["salat", "ærter", "radise",
+        "rabarber"]
 ];
+
+let frugtOgGront = []
 
 let myScore = 0
 
@@ -15,6 +17,8 @@ newGame()
 
 function newGame() {
     document.getElementById('svar').value = ""
+    frugtOgGront = getRandomItem(baseArray)
+    console.table(frugtOgGront)
     answer = getRandomItem(frugtOgGront)
     console.log(answer);
 }
@@ -31,36 +35,26 @@ function getRandomItem(myArray) {
 function guessCallback() {
 
 
-
-
-
-
-
     //--------------------------------------------------------------------
     /* du skal skrive denne del af  funktionen om, så brugeren bare skal gætte en af elementerne i arrayen, så det ikke er så svært!!*/
 
-    /*  if (userInput == answer) {
-         document.getElementById('feedback').innerHTML = `du har svarret ${userInput}..<br> og det er RIGTIGT!`
-     } else {
-         document.getElementById('feedback').innerHTML = `HA HA! forkert.<br> jeg tænkte på ${answer}`
-     } */
-
-
-    // toLowerCase er en string method, der konverterer alle bogstaver til smp, så man kan sammenligne dem uden at få fejl ved store og små bogstaver.
-
-    let userInput = document.getElementById('svar').value //.toLowerCase()
+    // toLowerCase er en string method, der konverterer alle bogstaver til små, så man kan sammenligne dem uden at få fejl ved store og små bogstaver.
+    let userInput = document.getElementById('svar').value.toLowerCase()
     answer = answer.toLowerCase()
 
-    console.log(frugtOgGront.includes(userInput));
 
-    if (frugtOgGront.includes(userInput)) {
+
+    if (userInput == answer) {
         document.getElementById('feedback').innerHTML = `du har svarret ${userInput}..<br> og det er RIGTIGT!`
+        myScore++// læg en til score
     } else {
         document.getElementById('feedback').innerHTML = `HA HA! forkert.<br> jeg tænkte på ${answer}`
+        myScore-- // træk en fra score
     }
 
     //--------------------------------------------------------------------
 
+    console.log(myScore);
 
     newGame()
 }
